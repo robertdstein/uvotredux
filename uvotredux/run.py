12e79cfb85cc4179e0eea24d19280de4628d8fb2
Robert Stein <rdstein@caltech.edu>
2024-03-08 15:32:27 -0800

    Basic CI

---
+++ uvotredux/run.py
@@ -0,0 +1,33 @@
+"""
+Wrapper script to run the swift reduction on a directory using command line arguments
+"""
+
+import argparse
+import logging
+
+from uvotredux.reduce import unpack_swift_directory
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    """
+    Function to run the swift reduction on a directory
+
+    :return: None
+    """
+
+    parser = argparse.ArgumentParser(description="Unpack a Swift UVOT observation")
+    parser.add_argument(
+        "-d", "--swift_obs_dir", help="Path to the base Swift observation directory"
+    )
+    parser.add_argument(
+        "-s", "--src_region_path", help="Path to the source region file"
+    )
+    parser.add_argument(
+        "-b", "--bkg_region_path", help="Path to the background region file"
+    )
+    args = parser.parse_args()
+    unpack_swift_directory(
+        args.swift_obs_dir, args.src_region_path, args.bkg_region_path
+    )
