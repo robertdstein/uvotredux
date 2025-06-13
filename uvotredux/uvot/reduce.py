dcbf9349631964c378b259e0d556639c92efcb76
Robert Stein <rdstein@umd.edu>
2025-06-13 11:56:38 -0400

    Clean up

--- uvotredux/uvot/reduce.py
+++ uvotredux/uvot/reduce.py
@@ -14,6 +14,63 @@ from uvotredux.uvot.filters import filter_dict
 logger = logging.getLogger(__name__)
 
 
+def execute_command(
+    cmd: str,
+    output_path: Path,
+    overwrite: bool = False,
+):
+    """
+    Function to execute a command and handle the output
+
+    :param cmd: Command to execute
+    :param output_path: Output path for the command
+    :param overwrite: Bool to overwrite existing files
+    :return: None
+    """
+    if output_path.is_file() and overwrite:
+        logger.info(f"Removing existing UVOT file: {output_path}")
+        output_path.unlink()
+
+    if output_path.is_file():
+        logger.info(f"UVOT file already exists: {output_path}")
+    else:
+        logger.info(f"Executing command: '{cmd}'")
+        subprocess.run(cmd, shell=True, check=True)
+        logger.info(f"UVOT file created at: {output_path}")
+
+    if not output_path.is_file():
+        logger.error(f"UVOT file not created: {output_path}")
+        logger.error(f"Command: {cmd}")
+
+
+def unpack_uvot_images(
+    uvot_dir: Path,
+) -> list[Path]:
+    """
+    Function to unpack the swift UVOT images
+
+    :param uvot_dir: Path to the UVOT directory
+    :return: List of unpacked images
+    """
+    swift_images = [x for x in uvot_dir.glob("*_sk.img") if x.is_file()]
+    swift_compressed_images = [x for x in uvot_dir.glob("*_sk.img.gz") if x.is_file()]
+
+    logger.info(f"Unpacking Swift observation: {uvot_dir.parent.parent}")
+    logger.info(f"Found {len(swift_compressed_images)} compressed images")
+
+    # Uncompress the images
+    for image in swift_compressed_images:
+        uncompressed_image = image.with_suffix("")
+        if not uncompressed_image.is_file():
+            logger.info(f"Uncompressing image: {image}")
+            with gzip.open(image, "rb") as f_in:
+                with open(uncompressed_image, "wb") as f_out:
+                    f_out.write(f_in.read())
+            swift_images.append(uncompressed_image)
+
+    return swift_images
+
+
 def unpack_single_uvot_obs(
     swift_obs_dir: Path,
     src_region_path: Path,
@@ -31,48 +88,26 @@ def unpack_single_uvot_obs(
     """
     uvot_dir = swift_obs_dir / "uvot/image"
 
-    swift_images = [x for x in uvot_dir.glob("*_sk.img") if x.is_file()]
-    swift_compressed_images = [x for x in uvot_dir.glob("*_sk.img.gz") if x.is_file()]
-
-    logger.info(f"Unpacking Swift observation: {swift_obs_dir}")
-
-    logger.info(f"Found {len(swift_compressed_images)} compressed images")
-
-    for image in swift_compressed_images:
-        uncompressed_image = image.with_suffix("")
-        if not uncompressed_image.is_file():
-            logger.info(f"Uncompressing image: {image}")
-            with gzip.open(image, "rb") as f_in:
-                with open(uncompressed_image, "wb") as f_out:
-                    f_out.write(f_in.read())
-            swift_images.append(uncompressed_image)
+    swift_images = unpack_uvot_images(uvot_dir)
 
     logger.info(f"Found {len(swift_images)} images")
 
     for image in swift_images:
         uvot_filter = filter_dict[image.name[14:16]]
         uvot_save_path = uvot_dir / f"{uvot_filter}.fits"
-        cmd = f"uvotimsum {image} {uvot_save_path}"
 
-        if uvot_save_path.is_file() and overwrite:
-            logger.info(f"Removing existing UVOT image: {uvot_save_path}")
-            uvot_save_path.unlink()
-
-        if uvot_save_path.is_file():
-            logger.info(f"UVOT image already exists: {uvot_save_path}")
-        else:
-            logger.info(f"Executing command: '{cmd}'")
-            try:
-                subprocess.run(cmd, shell=True, check=True)
-                logger.info(f"UVOT image created at: {uvot_save_path}")
-            except subprocess.CalledProcessError as e:
-                logger.error(f"Error creating UVOT image: {e}")
-                continue
+        try:
+            execute_command(
+                cmd=f"uvotimsum {image} {uvot_save_path}",
+                output_path=uvot_save_path,
+                overwrite=overwrite,
+            )
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error creating UVOT image: {e}")
+            continue
 
         if not uvot_save_path.is_file():
-            logger.error(f"UVOT image not created: {uvot_save_path}")
-            logger.error(f"Command: {cmd}")
-            return
+            continue
 
         output_path = uvot_dir / f"{uvot_filter}.out"
 
@@ -82,22 +117,8 @@ def unpack_single_uvot_obs(
             f"syserr=yes output=ALL apercorr=CURVEOFGROWTH "
             f"> {output_path.with_suffix('.log')}"
         )
-
-        if output_path.is_file() and overwrite:
-            logger.info(f"Removing existing UVOT source data: {output_path}")
-            output_path.unlink()
-
-        if output_path.is_file():
-            logger.info(f"UVOT source data already exists: {output_path}")
-        else:
-            logger.info(f"Executing command: '{cmd}'")
-            try:
-                subprocess.run(cmd, shell=True, check=True)
-                logger.info(f"UVOT source data created at: {output_path}")
-            except subprocess.CalledProcessError as e:
-                logger.error(f"Error creating UVOT source data: {e}")
-                continue
-
-        if not output_path.is_file():
-            logger.error(f"UVOT source data not created: {output_path}")
-            logger.error(f"Command: {cmd}")
+        try:
+            execute_command(cmd, output_path, overwrite=overwrite)
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error creating UVOT source data: {e}")
+            continue
